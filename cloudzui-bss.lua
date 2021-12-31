c95c7da645ea936a3ad0eb449dad82be445f51d1
Not-Cloudz <96899370+Not-Cloudz@users.noreply.github.com>
2021-12-31 14:01:28 +0700

    Update cloudzui-bss.lua

--- cloudzui-bss.lua
+++ cloudzui-bss.lua
@@ -1,1 +1,17 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Not-Cloudz/LuaScript/master/uilib.lua"))()
+local api = loadstring(game:HttpGet("https://raw.githubusercontent.com/not-weuz/xlpapi/main/api.lua"))()
 
+local CloudzUIWindow = library.new(true)
+CloudzUIWindow.ChangeToggleKey(Enum.KeyCode.Semicolon)
+
+local CreditsCategory = CloudzUIWindow:Category("Credits")
+
+-- Credits Category
+local Credits = CreditsCategory:Sector("Information")
+Credits:Cheat("Label", "-- Thank you for using our script, "..api.nickname)
+Credits:Cheat("Label", "-- Script version: 1.0 ")
+Credits:Cheat("Label", "-- Place version: "..game.PlaceVersion)
+Credits:Cheat("Label", "-- Hide GUI Button: ;")
+Credits:Cheat("Label", "-- UI Library : Finity")
+Credits:Cheat("Label", "-- API by weuz_ and davidshavrov)
+Credits:Cheat("Label", "-- Script by Cloudz")
